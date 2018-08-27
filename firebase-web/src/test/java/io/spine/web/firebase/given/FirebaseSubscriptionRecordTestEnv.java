b5b95f3b218f1f0b05ca6d6c73e7d982b07bc490
mdrachuk <mdrachuk@projects.tm>
2018-08-27 17:09:56 +0300

    Cover SubscriptionRecord with tests.

---
+++ firebase-web/src/test/java/io/spine/web/firebase/given/FirebaseSubscriptionRecordTestEnv.java
@@ -0,0 +1,202 @@
+/*
+ * Copyright 2018, TeamDev. All rights reserved.
+ *
+ * Redistribution and use in source and/or binary forms, with or without
+ * modification, must retain the above copyright notice and the following
+ * disclaimer.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package io.spine.web.firebase.given;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.MutableData;
+import com.google.firebase.database.Transaction;
+import io.spine.client.QueryResponse;
+import io.spine.client.QueryResponseVBuilder;
+import io.spine.core.Response;
+import io.spine.core.ResponseVBuilder;
+import io.spine.people.PersonName;
+import io.spine.people.PersonNameVBuilder;
+
+import java.util.List;
+import java.util.concurrent.CompletionStage;
+import java.util.function.Consumer;
+
+import static com.google.common.collect.Lists.newArrayList;
+import static io.spine.base.Identifier.newUuid;
+import static io.spine.core.Responses.statusOk;
+import static io.spine.json.Json.toCompactJson;
+import static io.spine.protobuf.AnyPacker.pack;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doAnswer;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+/**
+ * @author Mykhailo Drachuk
+ * @see io.spine.web.firebase.FirebaseSubscriptionRecordTest
+ */
+public final class FirebaseSubscriptionRecordTestEnv {
+
+    /**
+     * Prevents instantiation of this test environment.
+     */
+    private FirebaseSubscriptionRecordTestEnv() {
+    }
+
+    public static Book updateAuthors(Book designPatterns, Iterable<Author> gangOfFour) {
+        return designPatterns.toBuilder()
+                             .clearAuthors()
+                             .addAllAuthors(gangOfFour)
+                             .build();
+    }
+
+    public static MutableData mutableBookData(String key, Book book) {
+        MutableData mutableGuide = mock(MutableData.class);
+        when(mutableGuide.getValue()).thenReturn(toCompactJson(book));
+        when(mutableGuide.getKey()).thenReturn(key);
+        return mutableGuide;
+    }
+
+    public static void mockTransactionalWrite(DatabaseReference ref, MutableData mutableData) {
+        doAnswer(invocation -> {
+            Object[] arguments = invocation.getArguments();
+            Transaction.Handler handler = (Transaction.Handler) arguments[0];
+            handler.doTransaction(mutableData);
+            handler.onComplete(null, true, mock(DataSnapshot.class));
+            //noinspection ReturnOfNull returned by a void invokation
+            return null;
+        }).when(ref)
+          .runTransaction(any());
+    }
+
+    public static void mockQueryResponse(CompletionStage<QueryResponse> queryResponse,
+                                         Book... books) {
+        doAnswer(invocation -> {
+            Object[] arguments = invocation.getArguments();
+            @SuppressWarnings("unchecked")
+            Consumer<QueryResponse> consumer = (Consumer<QueryResponse>) arguments[0];
+            QueryResponseVBuilder responseBuilder = QueryResponseVBuilder.newBuilder()
+                                                                         .setResponse(okResponse());
+            for (Book book : books) {
+                responseBuilder.addMessages(pack(book));
+            }
+            consumer.accept(responseBuilder.build());
+            return mock(CompletionStage.class);
+        }).when(queryResponse)
+          .thenAcceptAsync(any());
+    }
+
+    public static final class Books {
+
+        public static Book designPatterns() {
+            return book(bookId(),
+                        "Design Patterns: Elements of Reusable Object-Oriented Software",
+                        Authors.literalGangOfFour());
+        }
+
+        public static Book donQuixote() {
+            BookId bookId = bookId();
+            return book(bookId, "Don Quixote", Authors.miguelDeCervantes());
+        }
+
+        public static Book aliceInWonderland() {
+            BookId bookId = bookId();
+            return book(bookId, "Alice’s Adventures in Wonderland", Authors.lewisCarrol());
+        }
+
+        public static Book guideToTheGalaxy() {
+            return book(bookId(), "The Hitchhiker's Guide to the Galaxy", Authors.douglasAdams());
+        }
+
+        private static Book book(BookId id, String name, Author author) {
+            return BookVBuilder.newBuilder()
+                               .setId(id)
+                               .setName(name)
+                               .addAuthors(author)
+                               .build();
+        }
+
+        private static BookId bookId() {
+            return BookIdVBuilder.newBuilder()
+                                 .setValue(newUuid())
+                                 .build();
+        }
+    }
+
+    public static final class Authors {
+
+        private static Author erichGamma() {
+            return author(name("Erich", "Gamma"));
+        }
+
+        private static Author richardHelm() {
+            return author(name("Richard", "Helm"));
+        }
+
+        private static Author ralphJohnson() {
+            return author(name("Ralph", "Johnson"));
+        }
+
+        private static Author johnVlissides() {
+            return author(name("John", "Vlissides"));
+        }
+
+        private static Author literalGangOfFour() {
+            PersonName name =
+                    PersonNameVBuilder.newBuilder()
+                                      .setGivenName("Gang of Four")
+                                      .build();
+            return author(name);
+        }
+
+        public static List<Author> gangOfFour() {
+            return newArrayList(erichGamma(), richardHelm(), ralphJohnson(),
+                                johnVlissides());
+        }
+
+        private static Author lewisCarrol() {
+            return author(name("Lewis", "Carrol"));
+        }
+
+        private static Author douglasAdams() {
+            return author(name("Douglas", "Adams"));
+        }
+
+        private static Author miguelDeCervantes() {
+            return author(name("Miguel", "de Cervantes"));
+        }
+
+        private static Author author(PersonName name) {
+            return AuthorVBuilder.newBuilder()
+                                 .setName(name)
+                                 .build();
+        }
+
+        private static PersonName name(String firstName, String lastName) {
+            return PersonNameVBuilder.newBuilder()
+                                     .setGivenName(firstName)
+                                     .setFamilyName(lastName)
+                                     .build();
+        }
+    }
+
+    private static Response okResponse() {
+        return ResponseVBuilder.newBuilder()
+                               .setStatus(statusOk())
+                               .build();
+    }
+}
