2089d5f5b0ba5ba43e232af17fdf0f93336b2744
mdrachuk <mdrachuk@projects.tm>
2018-08-30 22:26:49 +0300

    Use Type in backend tests.

--- client-js/test/client/firebase-backend-test.js
+++ client-js/test/client/firebase-backend-test.js
@@ -22,11 +22,13 @@ import assert from 'assert';
 import uuid from 'uuid';
 
 import {devFirebaseApp} from './test-firebase-app';
-import {TypedMessage, TypeUrl} from '../../src/client/typed-message';
+import {Type, TypedMessage, TypeUrl} from '../../src/client/typed-message';
 
 import {CreateTask, RenameTask} from '../../proto/test/js/spine/web/test/given/commands_pb';
-import {TaskId} from '../../proto/test/js/spine/web/test/given/task_pb';
+import {Task, TaskId} from '../../proto/test/js/spine/web/test/given/task_pb';
+import {ColumnFilter, CompositeColumnFilter} from 'spine-web-client-proto/spine/client/entities_pb';
 import {Topic} from '../../proto/test/js/spine/client/subscription_pb';
+import {Project} from '../../proto/test/js/spine/web/test/given/project_pb';
 import {BackendClient} from '../../src/client/backend-client';
 
 const MILLISECONDS = 1;
@@ -50,17 +52,17 @@ class Given {
     this.defaultTaskDescription = 'There seems to be a bug with the rings that needs to be fixed';
     this.TYPE = {
       OF_ENTITY: {
-        TASK: new TypeUrl('type.spine.io/spine.web.test.given.Task'),
-        PROJECT: new TypeUrl('type.spine.io/spine.web.test.given.Project')
+        TASK: new Type(Task, new TypeUrl('type.spine.io/spine.web.test.given.Task')),
+        PROJECT: new Type(Project, new TypeUrl('type.spine.io/spine.web.test.given.Project')),
       },
       OF_IDENTIFIER: {
-        TASK_ID: new TypeUrl('type.spine.io/spine.web.test.given.TaskId'),
+        TASK_ID: new Type(TaskId, new TypeUrl('type.spine.io/spine.web.test.given.TaskId')),
       },
       OF_COMMAND: {
-        CREATE_TASK: new TypeUrl('type.spine.io/spine.web.test.given.CreateTask'),
-        RENAME_TASK: new TypeUrl('type.spine.io/spine.web.test.given.RenameTask'),
+        CREATE_TASK: new Type(CreateTask, new TypeUrl('type.spine.io/spine.web.test.given.CreateTask')),
+        RENAME_TASK: new Type(RenameTask, new TypeUrl('type.spine.io/spine.web.test.given.RenameTask')),
       },
-      MALFORMED: new TypeUrl('types.spine.io/malformed')
+      MALFORMED: new Type(Object, new TypeUrl('types.spine.io/malformed')),
     };
     Given._deepFreeze(this);
   }
