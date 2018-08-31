c5223b668f3130a7844896c8f979bb00629711b7
mdrachuk <mdrachuk@projects.tm>
2018-08-31 13:56:52 +0300

    Add primitive value wrapper factories to TypedMessage.

--- client-js/src/client/typed-message.js
+++ client-js/src/client/typed-message.js
@@ -23,9 +23,14 @@
 import base64 from 'base64-js';
 import {Any} from 'spine-web-client-proto/google/protobuf/any_pb';
 import {
+  BoolValue,
+  DoubleValue,
+  FloatValue,
   Int32Value,
   Int64Value,
-  StringValue
+  StringValue,
+  UInt32Value,
+  UInt64Value,
 } from 'spine-web-client-proto/google/protobuf/wrappers_pb';
 import {WebQuery} from 'spine-web-client-proto/spine/web/web_query_pb';
 import {Subscription, Topic} from 'spine-web-client-proto/spine/client/subscription_pb';
@@ -81,7 +86,12 @@ export class Type {
 // PRIMITIVE WRAPPERS
 Type.STRING = new Type(StringValue, new TypeUrl('type.googleapis.com/proto.google.protobuf.StringValue'));
 Type.INT32 = new Type(Int32Value, new TypeUrl('type.googleapis.com/proto.google.protobuf.Int32Value'));
+Type.UINT32 = new Type(UInt32Value, new TypeUrl('type.googleapis.com/proto.google.protobuf.Int32Value'));
 Type.INT64 = new Type(Int64Value, new TypeUrl('type.googleapis.com/proto.google.protobuf.Int64Value'));
+Type.UINT64 = new Type(UInt64Value, new TypeUrl('type.googleapis.com/proto.google.protobuf.Int64Value'));
+Type.BOOL = new Type(BoolValue, new TypeUrl('type.googleapis.com/proto.google.protobuf.BoolValue'));
+Type.DOUBLE = new Type(DoubleValue, new TypeUrl('type.googleapis.com/proto.google.protobuf.DoubleValue'));
+Type.FLOAT = new Type(FloatValue, new TypeUrl('type.googleapis.com/proto.google.protobuf.FloatValue'));
 
 // SPINE WEB
 Type.WEB_QUERY = new Type(WebQuery, new TypeUrl('type.spine.io/spine.web.WebQuery'));
@@ -113,7 +123,7 @@ export class TypedMessage {
     this.message = message;
     this.type = type;
   }
-  
+
   /**
    * Converts this message into a Base64-encoded byte string.
    *
@@ -122,5 +132,99 @@ export class TypedMessage {
   toBase64() {
     const bytes = this.message.serializeBinary();
     return base64.fromByteArray(bytes);
+  }
+
+  /**
+   * Creates a new `TypedMessage` wrapping a string.
+   *
+   * @param {!String} value a string value for `TypedMessage`
+   * @return {TypedMessage<StringValue>} a new `TypedMessage` instance with provided value
+   */
+  static string(value) {
+    return new TypedMessage(new StringValue([value.toString()]), Type.STRING);
+  }
+
+  /**
+   * Creates a new `TypedMessage` with a 32-bit integer value.
+   *
+   * The number gets floored if the provided value contains a floating point.
+   *
+   * @param {!number|Number} value a number value for `TypedMessage`
+   * @return {TypedMessage<Int32Value>} a new `TypedMessage` instance with provided value
+   */
+  static int32(value) {
+    const message = new Int32Value([Math.floor(value.valueOf())]);
+    return new TypedMessage(message, Type.INT64);
+  }
+
+  /**
+   * Creates a new `TypedMessage` with an unsigned 32-bit integer value.
+   *
+   * The number gets floored if the provided value contains a floating point.
+   *
+   * @param {!number|Number} value a number value for `TypedMessage`
+   * @return {TypedMessage<UInt32Value>} a new `TypedMessage` instance with provided value
+   */
+  static uint32(value) {
+    const message = new UInt32Value([Math.floor(value.valueOf())]);
+    return new TypedMessage(message, Type.UINT64);
+  }
+
+  /**
+   * Creates a new `TypedMessage` with a 64-bit integer value.
+   *
+   * The number gets floored if the provided value contains a floating point.
+   *
+   * @param {!number|Number} value a number value for `TypedMessage`
+   * @return {TypedMessage<Int64Value>} a new `TypedMessage` instance with provided value
+   */
+  static int64(value) {
+    const message = new Int64Value([Math.floor(value.valueOf())]);
+    return new TypedMessage(message, Type.INT64);
+  }
+
+  /**
+   * Creates a new `TypedMessage` with an unsigned 64-bit integer value.
+   *
+   * The number gets floored if the provided value contains a floating point.
+   *
+   * @param {!number|Number} value a number value for `TypedMessage`
+   * @return {TypedMessage<UInt64Value>} a new `TypedMessage` instance with provided value
+   */
+  static uint64(value) {
+    const message = new UInt64Value([Math.floor(value.valueOf())]);
+    return new TypedMessage(message, Type.UINT64);
+  }
+
+  /**
+   * Creates a new `TypedMessage` with a float value.
+   *
+   * @param {!number|Number} value a number value for `TypedMessage`
+   * @return {TypedMessage<FloatValue>} a new `TypedMessage` instance with provided value
+   */
+  static float(value) {
+    const message = new FloatValue([value.valueOf()]);
+    return new TypedMessage(message, Type.FLOAT);
+  }
+
+  /**
+   * Creates a new `TypedMessage` with a double value.
+   *
+   * @param {!number|Number} value a number value for `TypedMessage`
+   * @return {TypedMessage<DoubleValue>} a new `TypedMessage` instance with provided value
+   */
+  static double(value) {
+    const message = new DoubleValue([value.valueOf()]);
+    return new TypedMessage(message, Type.DOUBLE);
+  }
+
+  /**
+   * Creates a new `TypedMessage` with a boolean value.
+   *
+   * @param {!boolean|Boolean} value `true` or `false` value for the `TypedMessage`
+   * @return {TypedMessage<BoolValue>} a new `TypedMessage` instance with provided value
+   */
+  static bool(value) {
+    return new TypedMessage(new BoolValue([value]), Type.BOOL);
   }
 }
