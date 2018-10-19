9787dbb4e3baea48af1b1dd9e05104e2624ea502
yegor.udovchenko <yegor.udovchenko@teamdev.com>
2018-10-19 19:58:29 +0300

    Fix mistake with wrong returning type in docs.

--- client-js/src/client/http-endpoint.js
+++ client-js/src/client/http-endpoint.js
@@ -180,8 +180,8 @@ export class HttpEndpoint extends Endpoint {
    * Sends off a command to the endpoint.
    *
    * @param {!TypedMessage<Command>} command a Command send to Spine server
-   * @return {Promise<JSON|SpineError>} a promise of a successful server response JSON data, rejected if
-   *                                    the client response is not 2xx or a connection error occurs
+   * @return {Promise<Object|SpineError>} a promise of a successful server response JSON data, rejected if
+   *                                      the client response is not 2xx or a connection error occurs
    * @protected
    */
   _executeCommand(command) {
@@ -192,8 +192,8 @@ export class HttpEndpoint extends Endpoint {
    * Sends off a query to the endpoint.
    *
    * @param {!TypedMessage<WebQuery>} webQuery a Query to Spine server to retrieve some domain entities
-   * @return {Promise<JSON|SpineError>} a promise of a successful server response JSON data, rejected if
-   *                                    the client response is not 2xx or a connection error occurs
+   * @return {Promise<Object|SpineError>} a promise of a successful server response JSON data, rejected if
+   *                                      the client response is not 2xx or a connection error occurs
    * @protected
    */
   _performQuery(webQuery) {
@@ -204,8 +204,8 @@ export class HttpEndpoint extends Endpoint {
    * Sends off a request to create a subscription for a topic.
    *
    * @param {!TypedMessage<spine.client.Topic>} topic a topic to subscribe to
-   * @return {Promise<JSON|SpineError>} a promise of a successful server response JSON data, rejected if
-   *                                    the client response is not 2xx or a connection error occurs
+   * @return {Promise<Object|SpineError>} a promise of a successful server response JSON data, rejected if
+   *                                      the client response is not 2xx or a connection error occurs
    * @protected
    */
   _subscribeTo(topic) {
@@ -217,8 +217,8 @@ export class HttpEndpoint extends Endpoint {
    *
    * @param {!TypedMessage<spine.client.Subscription>} subscription a subscription that is prevented
    *                                                                  from being closed by server
-   * @return {Promise<JSON|SpineError>} a promise of a successful server response JSON data, rejected if
-   *                                    the client response is not 2xx or a connection error occurs
+   * @return {Promise<Object|SpineError>} a promise of a successful server response JSON data, rejected if
+   *                                      the client response is not 2xx or a connection error occurs
    * @protected
    */
   _keepUp(subscription) {
@@ -229,8 +229,8 @@ export class HttpEndpoint extends Endpoint {
    * Sends off a request to cancel a subscription.
    *
    * @param {!TypedMessage<spine.client.Subscription>} subscription a subscription to be canceled
-   * @return {Promise<JSON|SpineError>} a promise of a successful server response JSON data, rejected if
-   *                                    the client response is not 2xx or a connection error occurs
+   * @return {Promise<Object|SpineError>} a promise of a successful server response JSON data, rejected if
+   *                                      the client response is not 2xx or a connection error occurs
    * @protected
    */
   _cancel(subscription) {
@@ -242,8 +242,8 @@ export class HttpEndpoint extends Endpoint {
    *
    * @param {!string} endpoint an endpoint to send the message to
    * @param {!TypedMessage} message a message to send, as a {@link TypedMessage}
-   * @return {Promise<JSON|SpineError>} a promise of a successful server response JSON data, rejected if
-   *                                    the client response is not 2xx or a connection error occurs
+   * @return {Promise<Object|SpineError>} a promise of a successful server response JSON data, rejected if
+   *                                      the client response is not 2xx or a connection error occurs
    * @private
    */
   _sendMessage(endpoint, message) {
@@ -256,11 +256,12 @@ export class HttpEndpoint extends Endpoint {
   }
 
   /**
-   * Retrieves the JSON data from the given response if it was successful, rejects with a respective error otherwise.
+   * Retrieves the JSON data from the given response if it was successful, rejects
+   * with a respective error otherwise.
    *
    * @param {!Response} response an HTTP request response
-   * @return {Promise<JSON|SpineError>} a promise of a successful server response JSON data, rejected if
-   *                                    the client response is not 2xx or if JSON parsing fails
+   * @return {Promise<Object|SpineError>} a promise of a successful server response JSON data, rejected if
+   *                                      the client response is not 2xx or if JSON parsing fails
    * @private
    */
   static _jsonOrError(response) {
@@ -280,8 +281,8 @@ export class HttpEndpoint extends Endpoint {
    * Parses the given response JSON data, rejects if parsing fails.
    *
    * @param {!Response} response an HTTP request response
-   * @return {Promise<*|SpineError>} a promise of a server response parsing to be fulfilled with a JSON
-   *       data or rejected with {@link SpineError} if JSON parsing fails.
+   * @return {Promise<Object|SpineError>} a promise of a server response parsing to be fulfilled with a JSON
+   *                                      data or rejected with {@link SpineError} if JSON parsing fails.
    * @private
    */
   static _parseJson(response) {
