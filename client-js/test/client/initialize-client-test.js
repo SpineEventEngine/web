5470ba421400a05fc13271862512a7479acb4fe1
yegor.udovchenko <yegor.udovchenko@teamdev.com>
2019-01-29 16:47:28 +0200

    Add tests for `initializeClient` function.

---
+++ client-js/test/client/initialize-client-test.js
@@ -0,0 +1,141 @@
+/*
+ * Copyright 2018, TeamDev. All rights reserved.
+ *
+ * Redistribution and use in source and/or binary forms, with or without
+ * modification, must retain the above copyright notice and the following
+ * disclaimer.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+import assert from 'assert';
+
+import {devFirebaseDatabase} from './test-firebase-database';
+
+import * as testProtobuf from '@testProto/index';
+import {initializeClient} from '@lib/index';
+import {ActorProvider} from '@lib/client/actor-request-factory';
+import {UserId} from '@proto/spine/core/user_id_pb';
+import {MockClient} from './test-helpers';
+import {FirebaseClient} from '@lib/client/firebase-client';
+import KnownTypes from '@lib/client/known-types';
+
+class Given {
+
+  constructor() {
+    throw new Error('A utility Given class cannot be instantiated.');
+  }
+
+  /**
+   * Options required for `FirebaseClient` initialization.
+   *
+   * @return {ClientOptions}
+   */
+  static firebaseClientOptions() {
+    return {
+      protoIndexFiles: Given.PROTO_FILES,
+      endpointUrl: Given.ENDPOINT_URL,
+      firebaseDatabase: devFirebaseDatabase,
+      actorProvider: Given.ACTOR_PROVIDER
+    };
+  }
+
+  /**
+   * Options required for custom client initialization.
+   *
+   * @return {ClientOptions}
+   */
+  static customClientOptions() {
+    return {
+      protoIndexFiles: Given.PROTO_FILES,
+      implementation: Given.MOCK_CLIENT
+    }
+  }
+
+  /**
+   * Checks whether all types from given proto files were registered in `KnownTypes`.
+   *
+   * @return {boolean}
+   */
+  static protoFilesRegistered() {
+    const protoIndexFile = Given.PROTO_FILES[0];
+    const typeUrlIterator = protoIndexFile.types.keys();
+
+    let typeUrl = typeUrlIterator.next();
+    let isRegistered = true;
+
+    while (!typeUrl.done) {
+      if (!KnownTypes.hasType(typeUrl.value)) {
+        isRegistered = false;
+        break;
+      }
+      typeUrl = typeUrlIterator.next();
+    }
+
+    return isRegistered;
+  }
+}
+
+Given.PROTO_FILES = [testProtobuf];
+Given.ENDPOINT_URL = 'http://no-reply-server.appspot.com';
+Given.ACTOR_PROVIDER = new ActorProvider();
+Given.MOCK_CLIENT = new MockClient();
+
+describe('`initializeClient`', function () {
+
+  it('should register types defined in proto files', () => {
+    KnownTypes.clear();
+    assert.ok(!Given.protoFilesRegistered());
+    initializeClient(Given.customClientOptions());
+
+    assert.ok(Given.protoFilesRegistered());
+  });
+
+  it('should create a FirebaseClient instance for the correct options', () => {
+    const client = initializeClient(Given.firebaseClientOptions());
+    assert.ok(client instanceof FirebaseClient);
+  });
+
+  it('should return the provided `Client` implementation for the correct options', () => {
+    const client = initializeClient(Given.customClientOptions());
+    assert.ok(client === Given.MOCK_CLIENT);
+  });
+
+  it('should throw an error when provided `Client` implementation does not extend `Client` ', () => {
+    const optionsWithMalformedClient = Object.assign(Given.customClientOptions(), {implementation: {}});
+    assert.throws(() => {
+      initializeClient(optionsWithMalformedClient);
+    });
+  });
+
+  it('should throw an error when proto files missing', () => {
+    const optionsWithNoProtoFiles = Object.assign(Given.firebaseClientOptions(), {protoIndexFiles: []});
+    assert.throws(() => {
+      initializeClient(optionsWithNoProtoFiles);
+    });
+  });
+
+  it('should throw an error when endpoint URL missing', () => {
+    const optionsWithNoEndpointUrl = Object.assign(Given.firebaseClientOptions(), {endpointUrl: undefined});
+    assert.throws(() => {
+      initializeClient(optionsWithNoEndpointUrl);
+    });
+  });
+
+  it('should throw an error when actor provider missing', () => {
+    const optionsWithNoActorProvider = Object.assign(Given.firebaseClientOptions(), {actorProvider: undefined});
+      assert.throws(() => {
+        initializeClient(optionsWithNoActorProvider);
+      });
+  });
+});
