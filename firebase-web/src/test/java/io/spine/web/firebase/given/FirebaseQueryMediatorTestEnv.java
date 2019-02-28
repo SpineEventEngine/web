c5ceb6d3fb3d498af0b801e648e71c2788381458
dmitrykuzmin <dmitry.kuzmin@teamdev.com>
2019-02-28 17:17:19 +0200

    Fix formatting

--- firebase-web/src/test/java/io/spine/web/firebase/given/FirebaseQueryMediatorTestEnv.java
+++ firebase-web/src/test/java/io/spine/web/firebase/given/FirebaseQueryMediatorTestEnv.java
@@ -20,6 +20,7 @@
 
 package io.spine.web.firebase.given;
 
+import com.google.common.collect.ImmutableSet;
 import com.google.protobuf.Message;
 import io.grpc.stub.StreamObserver;
 import io.spine.client.EntityStateWithVersion;
@@ -32,7 +33,6 @@ import io.spine.core.Version;
 import java.util.ArrayList;
 import java.util.Collection;
 
-import static com.google.common.collect.ImmutableSet.copyOf;
 import static com.google.protobuf.Any.pack;
 import static io.spine.core.Responses.ok;
 import static java.util.stream.Collectors.toSet;
@@ -51,9 +51,10 @@ public final class FirebaseQueryMediatorTestEnv {
 
         public TestQueryService(Message... messages) {
             super();
-            this.response = copyOf(messages).stream()
-                                            .map(TestQueryService::toEntityState)
-                                            .collect(toSet());
+            this.response = ImmutableSet.copyOf(messages)
+                                        .stream()
+                                        .map(TestQueryService::toEntityState)
+                                        .collect(toSet());
         }
 
         @Override
