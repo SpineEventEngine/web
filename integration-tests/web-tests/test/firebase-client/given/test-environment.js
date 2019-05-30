e6029c251eb42e41e44ab6496caf1210922ff6a6
yegor.udovchenko <yegor.udovchenko@teamdev.com>
2019-05-30 15:46:48 +0300

    Prepare environment with tasks assigned to test users.

--- integration-tests/web-tests/test/firebase-client/given/test-environment.js
+++ integration-tests/web-tests/test/firebase-client/given/test-environment.js
@@ -26,6 +26,14 @@ import {UserTasks} from '@testProto/spine/web/test/given/user_tasks_pb';
 import {UserId} from '@testProto/spine/core/user_id_pb';
 import {Type} from '@lib/client/typed-message';
 
+/**
+ * @typedef {Object} User a type representing a user with his tasks for test purposes
+ *
+ * @property {UserId} id
+ * @property {String} name
+ * @property {TaskId[]} tasks
+ */
+
 export default class TestEnvironment {
 
     constructor() {
@@ -68,6 +76,43 @@ export default class TestEnvironment {
     }
 
     /**
+     * Creates two assigned tasks for each user in a given list.
+     *
+     * @param {User[]} users a list of users to create tasks for
+     * @param {Client} client a Spine client to send commands
+     * @return {Promise} a promise to be resolved when all `CreateTask` commands acknowledged;
+     *                   rejected if an error occurs;
+     */
+    static createTwoTasksFor(users, client) {
+        const createTaskPromises = [];
+        users.forEach(user => {
+            for (let i = 0; i < 2; i++) {
+                const command = TestEnvironment.createTaskCommand({
+                    named: `task#${i + 1}-for-${user.name}`,
+                    assignedTo: user.id
+                });
+                const taskId = command.getId();
+
+                let createTaskAcknowledged;
+                let createTaskFailed;
+
+                const promise = new Promise((resolve, reject) => {
+                    createTaskAcknowledged = resolve;
+                    createTaskFailed = reject;
+                });
+                createTaskPromises.push(promise);
+                client.sendCommand(command, () => {
+                    user.tasks.push(taskId);
+                    createTaskAcknowledged();
+
+                }, createTaskFailed);
+            }
+        });
+
+        return Promise.all(createTaskPromises);
+    }
+
+    /**
      * @param {{
      *     withId!: String,
      *     to!: String
@@ -106,11 +151,24 @@ export default class TestEnvironment {
 
     /**
      * @param {?String} withPrefix
+     * @return {UserId}
      */
     static userId(withPrefix) {
         const id = new UserId();
         id.setValue(`${withPrefix ? withPrefix : 'ANONYMOUS'}-${uuid.v4()}`);
         return id;
+    }
+
+    /**
+     * @param {?String} withName
+     * @return {User}
+     */
+    static newUser(withName) {
+        return {
+            name: withName,
+            id: TestEnvironment.userId(withName),
+            tasks: []
+        }
     }
 
     /**
