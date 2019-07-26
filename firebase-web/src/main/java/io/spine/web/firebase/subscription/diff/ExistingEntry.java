bb58e3148dfcc4ba8474164377702392533ce87d
Dmytro Dashenkov <dmytro.dashenkov@teamdev.com>
2019-07-26 16:25:02 +0300

    Better doc

--- firebase-web/src/main/java/io/spine/web/firebase/subscription/diff/ExistingEntry.java
+++ firebase-web/src/main/java/io/spine/web/firebase/subscription/diff/ExistingEntry.java
@@ -2,6 +2,7 @@ package io.spine.web.firebase.subscription.diff;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.google.gson.JsonObject;
+import org.checkerframework.checker.nullness.qual.Nullable;
 
 import java.util.List;
 
@@ -15,23 +16,20 @@ final class ExistingEntry {
     private final String key;
     private final String data;
     private final JsonNode json;
-    private final JsonNode id;
-    private final boolean containsId;
+    private final @Nullable JsonNode id;
 
     private ExistingEntry(String key, String data) {
         this.key = key;
         this.data = data;
         this.json = JsonParser.parse(data);
         this.id = json.get("id");
-        this.containsId = id != null;
     }
 
     static List<ExistingEntry> fromJson(JsonObject object) {
         return object
                 .entrySet()
                 .stream()
-                .map(entry -> new ExistingEntry(entry.getKey(), entry.getValue()
-                                                                     .getAsString()))
+                .map(entry -> new ExistingEntry(entry.getKey(), entry.getValue().getAsString()))
                 .collect(toList());
     }
 
@@ -57,11 +55,14 @@ final class ExistingEntry {
     }
 
     /**
-     * Checks if this entries {@code "id"} field matches the provided one.
+     * Checks if this entry's {@code "id"} field matches the provided one.
      *
-     * <p>Returns {@code false} if the ID is {@code null}.
+     * <p>Returns {@code false} if this entry has no {@code "id"} field
+     *
+     * @return {@code true} if the ID of this entry is equal to the given node,
+     *         {@code false} otherwise
      */
     boolean idEquals(JsonNode id) {
-        return containsId && this.id.equals(id);
+        return id.equals(this.id);
     }
 }
