7e445975549ef1c9ff0b096664fb02a30e32c9b6
Dmytro Dashenkov <dmytro.dashenkov@teamdev.com>
2019-09-18 19:03:17 +0300

    Add an extra empty line

--- client-dart/lib/uuids.dart
+++ client-dart/lib/uuids.dart
@@ -23,6 +23,7 @@ import 'package:uuid/uuid.dart';
 /// Creates a new UUID of version 4.
 ///
 /// If [prefix] is specified, the result string starts with the given prefix.
+///
 String newUuid({String prefix = ''}) {
     var uuid = Uuid();
     return '${prefix}${uuid.v4()}';
