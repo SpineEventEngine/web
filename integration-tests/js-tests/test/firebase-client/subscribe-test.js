037f6c28c9c461bfc3e753d5ed71bea98d565bb8
dmitrykuzmin <dmitry.kuzmin@teamdev.com>
2019-12-04 21:14:32 +0200

    Add more integration tests for subscriptions

--- integration-tests/js-tests/test/firebase-client/subscribe-test.js
+++ integration-tests/js-tests/test/firebase-client/subscribe-test.js
@@ -21,8 +21,12 @@
 import assert from 'assert';
 import {fail} from '../test-helpers';
 import TestEnvironment from './given/test-environment';
+import {TaskRenamed} from '@testProto/spine/web/test/given/events_pb';
 import {Task} from '@testProto/spine/web/test/given/task_pb';
 import {client} from './given/firebase-client';
+import {Filters} from '@lib/client/actor-request-factory';
+import {AnyPacker} from '@lib/client/any-packer';
+import {Type} from '@lib/client/typed-message';
 
 describe('FirebaseClient subscription', function () {
 
@@ -134,7 +138,7 @@ describe('FirebaseClient subscription', function () {
             .catch(fail(done));
     });
 
-    it('retrieves updates by id', done => {
+    it('retrieves updates by ID', done => {
         const expectedChangesCount = 2;
         const INITIAL_TASK_NAME = 'Initial task name';
         const UPDATED_NAMES = ['Renamed once', 'Renamed twice'];
@@ -212,7 +216,126 @@ describe('FirebaseClient subscription', function () {
             .catch(fail(done));
     });
 
-    it('fails for a malformed entity type', done => {
+    // TODO:2019-11-27:dmytro.kuzmin:WIP Resolve presence/absence of backticks in string messages,
+    //  formatting of chained method calls, at least in the modified files.
+    it('is notified when the entity no longer matches the subscription criteria', done => {
+        const initialTaskName = 'Initial task name';
+        const nameAfterRenamed = 'Renamed task';
+
+        const createCommand = TestEnvironment.createTaskCommand({
+            withPrefix: 'spine-web-test-subscribe',
+            named: initialTaskName
+        });
+        const taskIdValue = createCommand.getId().getValue();
+
+        client.subscribeTo(Task)
+            .where(Filters.eq("name", initialTaskName))
+            .post()
+            .then(({itemAdded, itemChanged, itemRemoved, unsubscribe}) => {
+                itemAdded.subscribe({
+                    next: item => {
+                        const id = item.getId().getValue();
+                        console.log(`Retrieved new task '${id}'.`);
+                        if (taskIdValue === id) {
+                            assert.equal(
+                                initialTaskName, item.getName(),
+                                `Task is named "${item.getName()}", 
+                                expected "${initialTaskName}".`
+                            );
+                        }
+                        const renameCommand = TestEnvironment.renameTaskCommand({
+                            withId: taskIdValue,
+                            to: nameAfterRenamed
+                        });
+                        client.command(renameCommand)
+                            .onOk(() => console.log(
+                                `Task '${taskIdValue}' is renamed to '${nameAfterRenamed}'.`))
+                            .onError(fail(done, 'Unexpected error while renaming a task.'))
+                            .onRejection(fail(done, 'Unexpected rejection while renaming a task.'))
+                            .post();
+                    }
+                });
+                itemRemoved.subscribe({
+                    next: item => {
+                        const id = item.getId().getValue();
+                        console.log('Task removed');
+                        assert.equal(
+                            taskIdValue, id,
+                            `A wrong Task item is removed, expected the task with ID 
+                            "${taskIdValue}", received the task with ID "${id}".`
+                        );
+                        unsubscribe();
+                        done();
+                    }
+                });
+                itemChanged.subscribe({
+                    next: fail(done, 'The `itemChanged` call is unexpected within this test.')
+                });
+                client.command(createCommand)
+                    .onOk(() => console.log(`Task '${taskIdValue}' created.`))
+                    .onError(fail(done, 'Unexpected error while creating a task.'))
+                    .onRejection(fail(done, 'Unexpected rejection while creating a task.'))
+                    .post();
+            })
+            .catch(fail(done));
+    });
+
+    it('retrieves event updates', done => {
+        const initialTaskName = "The initial task name";
+        const updatedTaskName = "Renamed task";
+
+        let taskId;
+
+        client.subscribeToEvent(TaskRenamed)
+            .where(Filters.eq("name", updatedTaskName))
+            .post()
+            .then(({eventEmitted, unsubscribe}) => {
+                eventEmitted.subscribe({
+                   next: event => {
+                       const packedMessage = event.getMessage();
+                       const taskRenamedType = Type.forClass(TaskRenamed);
+                       const message = AnyPacker.unpack(packedMessage).as(taskRenamedType);
+                       const theTaskId = message.getId().getValue();
+                       assert.equal(
+                         taskId, theTaskId,
+                         `Expected the task ID to be ${taskId}, got ${theTaskId} instead.`
+                       );
+                       const newTaskName = message.getName();
+                       assert.equal(
+                           updatedTaskName, newTaskName,
+                           `Expected the new task name to be ${updatedTaskName}, got 
+                           ${newTaskName} instead.`
+                       );
+                       unsubscribe();
+                       done();
+                   }
+                });
+            });
+
+        const createCommand = TestEnvironment.createTaskCommand({
+            withPrefix: 'spine-web-test-subscribe',
+            named: initialTaskName
+        });
+        taskId = createCommand.getId().getValue();
+        client.command(createCommand)
+            .onOk(() => console.log(`Task '${createCommand.getId().getValue()}' created.`))
+            .onError(fail(done, 'Unexpected error while creating a task.'))
+            .onRejection(fail(done, 'Unexpected rejection while creating a task.'))
+            .post();
+
+        const renameCommand = TestEnvironment.renameTaskCommand({
+            withId: taskId,
+            to: updatedTaskName
+        });
+        client.command(renameCommand)
+            .onOk(() => console.log(`Task '${taskId}' renamed.`))
+            .onError(fail(done, 'Unexpected error while renaming a task.'))
+            .onRejection(fail(done,
+                'Unexpected rejection while renaming a task.'))
+            .post();
+    });
+
+    it('fails for a malformed type', done => {
         const Unknown = class {
             static typeUrl() {
                 return 'spine.web/fails.malformed.type'
