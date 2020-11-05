76d064c22a26d8825a86f903f3c88e6428b12467
Yuri Sergiichuk <yuri.sergiichuk@teamdev.com>
2020-11-05 16:52:56 +0200

    Add webpack-test configuration for integration tests module.

---
+++ integration-tests/js-tests/webpack-test.config.js
@@ -0,0 +1,38 @@
+/*
+ * Copyright 2020, TeamDev. All rights reserved.
+ *
+ * Redistribution and use in source and/or binary forms, with or without
+ * modification, must retain the above copyright notice and the following
+ * disclaimer.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+// This script provides a way to resolve Babel-like modules while using a JetBrains IDE.
+//
+// One should point the IDE Webpack configuration to this file in order to receive same aliases
+// as configured in the `.babelrc`.
+//
+// The resolved aliases below must be always in sync with the `.babelrc` content.
+//
+
+const path = require('path');
+
+module.exports = {
+    resolve: {
+        alias: {
+            '@testProto': path.resolve(__dirname, './test/proto'),
+            '@lib': path.resolve(__dirname, './node_modules/spine-web')
+        }
+    }
+}
