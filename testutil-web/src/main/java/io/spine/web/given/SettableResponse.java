8ef15dd4e5cf30de5a63f5000accb9d5a46cf7f9
Yuri Sergiichuk <yuri.sergiichuk@teamdev.com>
2020-12-04 17:30:53 +0200

    Introduce a `SettableResponse` that allows verifying particular interactions done with a response.

---
+++ testutil-web/src/main/java/io/spine/web/given/SettableResponse.java
@@ -0,0 +1,97 @@
+/*
+ * Copyright 2020, TeamDev. All rights reserved.
+ *
+ * Redistribution and use in source and/or binary forms, with or without
+ * modification, must retain the above copyright notice and the following
+ * disclaimer.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package io.spine.web.given;
+
+import com.google.common.collect.ImmutableSet;
+import org.checkerframework.checker.nullness.qual.Nullable;
+
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * A mocked response whose {@code status} and {@code type} could be set.
+ */
+public final class SettableResponse implements MockedResponse {
+
+    private final StringWriter writer = new StringWriter();
+    private final Map<String, String> headers = new HashMap<>();
+    private String contentType = "";
+    private int status = -1;
+
+    @Override
+    public int getStatus() {
+        return status;
+    }
+
+    @Override
+    public @Nullable String getHeader(String name) {
+        return headers.get(name);
+    }
+
+    @Override
+    public void sendError(int sc) {
+        status = sc;
+    }
+
+    @Override
+    public void sendError(int sc, String msg) {
+        sendError(sc);
+    }
+
+    @Override
+    public void setContentType(String contentType) {
+        this.contentType = contentType;
+    }
+
+    @Override
+    public @Nullable String getContentType() {
+        return contentType;
+    }
+
+    @Override
+    public ImmutableSet<String> getHeaders(String name) {
+        String result = headers.get(name);
+        if (result == null) {
+            return ImmutableSet.of();
+        }
+        return ImmutableSet.of(result);
+    }
+
+    @Override
+    public Set<String> getHeaderNames() {
+        return headers.keySet();
+    }
+
+    @Override
+    public PrintWriter getWriter() {
+        return new PrintWriter(writer);
+    }
+
+    /**
+     * Returns the content of the associated response writer.
+     */
+    public String writerContent() {
+        return writer.toString();
+    }
+}
