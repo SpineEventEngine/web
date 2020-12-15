b68edcfdb9abfcf8b545a8c8332c67f3cacc2ed5
Yuri Sergiichuk <yuri.sergiichuk@teamdev.com>
2020-12-15 12:50:05 +0200

    Update version to 1.7.0

--- version.gradle.kts
+++ version.gradle.kts
@@ -18,10 +18,10 @@
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-val spineBaseVersion: String by extra("1.6.16")
-val spineTimeVersion: String by extra("1.6.16")
-val spineCoreVersion: String by extra("1.6.19")
+val spineBaseVersion: String by extra("1.7.0")
+val spineTimeVersion: String by extra("1.7.0")
+val spineCoreVersion: String by extra("1.7.0")
 val spineVersion: String by extra(spineCoreVersion)
 
-val versionToPublish: String by extra("1.6.17")
+val versionToPublish: String by extra("1.7.0")
 val versionToPublishJs: String by extra(versionToPublish)
