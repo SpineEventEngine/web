5a24c8377bd72066553e781dbd8047a508ecb678
Alex Tymchenko <alex.tymchenko@teamdev.com>
2023-02-07 12:59:14 +0000

    Bump the version to `1.9.0-SNAPSHOT.12`.

--- version.gradle.kts
+++ version.gradle.kts
@@ -29,5 +29,5 @@ val spineTimeVersion: String by extra("1.9.0-SNAPSHOT.5")
 val spineCoreVersion: String by extra("1.9.0-SNAPSHOT.6")
 val spineVersion: String by extra(spineCoreVersion)
 
-val versionToPublish: String by extra("1.9.0-SNAPSHOT.11")
+val versionToPublish: String by extra("1.9.0-SNAPSHOT.12")
 val versionToPublishJs: String by extra(versionToPublish)
