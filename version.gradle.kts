b41db872b8c91e66b8f8d1d619dd96893297bac3
Alex Tymchenko <alex.tymchenko@teamdev.com>
2023-05-19 11:50:02 +0100

    Bump to `1.9.0`.

--- version.gradle.kts
+++ version.gradle.kts
@@ -24,10 +24,10 @@
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-val spineBaseVersion: String by extra("1.9.0-SNAPSHOT.6")
-val spineTimeVersion: String by extra("1.9.0-SNAPSHOT.6")
-val spineCoreVersion: String by extra("1.9.0-SNAPSHOT.13")
+val spineBaseVersion: String by extra("1.9.0")
+val spineTimeVersion: String by extra("1.9.0")
+val spineCoreVersion: String by extra("1.9.0")
 val spineVersion: String by extra(spineCoreVersion)
 
-val versionToPublish: String by extra("1.9.0-SNAPSHOT.13")
+val versionToPublish: String by extra("1.9.0")
 val versionToPublishJs: String by extra(versionToPublish)
