7aa0353be52187563c3f3f68dbb2290eebe16512
Alex Tymchenko <alex.tymchenko@teamdev.com>
2023-10-06 12:07:51 +0100

    Update version -> `1.9.1`.

--- version.gradle.kts
+++ version.gradle.kts
@@ -29,5 +29,5 @@ val spineTimeVersion: String by extra("1.9.0")
 val spineCoreVersion: String by extra("1.9.0")
 val spineVersion: String by extra(spineCoreVersion)
 
-val versionToPublish: String by extra("1.9.0")
+val versionToPublish: String by extra("1.9.1")
 val versionToPublishJs: String by extra(versionToPublish)
